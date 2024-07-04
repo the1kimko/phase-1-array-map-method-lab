6068f34257391a32933a8e64be0afb688bf70093
KomfortKimko <komfortkimk@gmail.com>
2024-07-04 06:59:31 +0300

    Array map lab

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(callBack);
+}
+
+function callBack(array) {
+  return array.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
